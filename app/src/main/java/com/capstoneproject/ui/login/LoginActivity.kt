260ad7960ca0f4ee33829eaf4022825bed9df391
Muchammad Saifurrijaal <88447746+msaifurrijaal@users.noreply.github.com>
2024-05-22 20:55:41 +0700

    style: setup color theme (#1)

--- app/src/main/java/com/capstoneproject/ui/login/LoginActivity.kt
+++ app/src/main/java/com/capstoneproject/ui/login/LoginActivity.kt
@@ -8,7 +8,6 @@ import com.capstoneproject.ui.main.MainActivity
 import com.capstoneproject.databinding.ActivityLoginBinding
 
 
-
 class LoginActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityLoginBinding
