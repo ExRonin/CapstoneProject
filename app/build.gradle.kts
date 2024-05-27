365e1b420becb2f7fdff2864b6029e11a69335c1
ExRonin <palvido81@gmail.com>
2024-05-27 09:40:15 +0700

    fix: search,login,register

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -46,7 +46,7 @@ dependencies {
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.navigation:navigation-fragment-ktx:2.7.7")
     implementation("androidx.navigation:navigation-ui-ktx:2.7.7")
-    implementation("androidx.activity:activity:1.8.0")
+    implementation("androidx.activity:activity:1.9.0")
     implementation("androidx.compose.runtime:runtime-saved-instance-state:1.0.0-alpha11")
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
@@ -62,7 +62,7 @@ dependencies {
     implementation("com.google.android.gms:play-services-location:21.2.0")
     implementation ("com.google.android.gms:play-services-maps:18.2.0")
 
-    implementation("com.github.bumptech.glide:glide:4.15.0")
+    implementation("com.github.bumptech.glide:glide:4.16.0")
     implementation("com.github.smarteist:autoimageslider:1.4.0")
 
 
